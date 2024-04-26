4bb235bd0b492b1cf675a69f85f89a80606ab041
Ilya <me@redguy.ru>
2024-04-26 13:48:21 +0300

    Add bundle to ProjectCustomField

--- src/entities/projectCustomField.ts
+++ src/entities/projectCustomField.ts
@@ -8,8 +8,17 @@ export class ProjectCustomFieldImpl {
     isPublic: boolean = false;
     ordinal: number = 0;
     id: string = '';
+    bundle: StateBundle = new StateBundleImpl();
 }
 
 export interface ProjectCustomField extends ProjectCustomFieldImpl {
+
+}
+
+export class StateBundleImpl {
+    id: string = '';
+}
+
+export interface StateBundle extends StateBundleImpl {
 
 }
